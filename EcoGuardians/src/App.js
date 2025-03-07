cc79889e0c02e6389551dffb65a1e5567e48f6a1
Pedro Mendoza <A01772504@tec.mx>
2025-03-06 18:02:03 -0600

    Saleforce and Papypal back

--- EcoGuardians/src/App.js
+++ EcoGuardians/src/App.js
@@ -1,21 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import aw from './img/aw.jpeg';
 import './App.css';
-import Paypal from './Paypal';  // Importamos el componente de Paypal
+import Paypal from './Paypal';
+import SalesforceForm from './salesforce';
+import JuegoAwa from './juego';
 
 function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    axios.get('http://localhost:5000/api/data')  // URL del backend en Flask
-      .then(response => {
-        setData(response.data);
-      })
-      .catch(error => {
-        console.error('Hubo un error al obtener los datos:', error);
-      });
-  }, []);
 
   return (
     <div>
@@ -25,24 +15,13 @@ function App() {
         </div>
       </header>
 
-      {data ? <h1>{data.message}</h1> : <p>Cargando...</p>}
-
-      <div className="hola">
-        Hola a todos
-      </div>
-
-      <iframe 
-        src="https://ecoguardians2-0.github.io/EcoGuardians-2-0/" 
-        width="100%" 
-        height="600" 
-        style={{ border: "none" }}  
-        allowFullScreen 
-        title="EcoGuardians Website"
-      >
-      </iframe>
+      <JuegoAwa />
+      
 
       {/* Aquí se muestra el formulario de donación de PayPal después del iframe */}
       <Paypal />
+
+      <SalesforceForm />
     </div>
   );
 }
