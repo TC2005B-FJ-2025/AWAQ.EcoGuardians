f8ce3069bcf3d5eed4b2f4f55305053a2c9a518e
Marianaa112 <A01772501@tec.mx>
2025-04-11 21:55:14 -0600

    Esto es lo del tercer sprint, los botones conectados a los formularios

--- EcoGuardians/src/App.js
+++ EcoGuardians/src/App.js
@@ -7,6 +7,11 @@ import VideojuegoRegistrado from "./InHome/VideojuegoRegistrado";
 import BienvenidaComoInvitado from "./InHome/BienvenidaComoInvitado.js";
 import VideojuegoInvitado from "./InHome/VideojuegoInvitado.js";
 import PreguntasFrecuentes from "./FAQs/PreguntasFrecuentes.js"
+import Contacto from "./Formularios/Contacto.js"
+import Sponsors from "./Formularios/Sponsors.js"
+import Prospecto from "./Formularios/Prospecto.js"
+import CrearCaso from "./Formularios/CrearCaso.js"; 
+
 
 function App() {
   return (
@@ -19,6 +24,10 @@ function App() {
         <Route path="/juego" element={<VideojuegoRegistrado />} />
         <Route path="/juegoInvitado" element={<VideojuegoInvitado/>} />
         <Route path="/faqs" element={<PreguntasFrecuentes />} />
+        <Route path="/contacto" element={<Contacto />} />
+        <Route path="/Sponsors" element={<Sponsors />} />
+        <Route path="/Prospecto" element={<Prospecto />} />
+        <Route path="/CrearCaso" element={<CrearCaso />} />
         {/* Puedes agregar más rutas aquí */}
       </Routes>
     </BrowserRouter>
