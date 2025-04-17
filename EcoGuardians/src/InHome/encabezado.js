4351143ae18ee51fae6f089de6628e6d6b604f8d
Gabriel Esperilla <gabochu280@gmail.com>
2025-04-16 22:00:42 -0600

    feat: boton volver

--- EcoGuardians/src/InHome/encabezado.js
+++ EcoGuardians/src/InHome/encabezado.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom"; // Importa useNavigate de react-router-dom
 import { Link } from "react-router-dom"; // Importa Link de react-router-dom
 import { Smile } from "lucide-react"; // implementación de lucide-react para el icono de la carita feliz
 import LogoHeader from "../componentes/LogoHeader.png";
 
-const Encabezado = () => {
+const Encabezado = ({ onHome }) => {
   const [idioma, setIdioma] = useState("ES");
   const [mostrarDropdown, setMostrarDropdown] = useState(false);
+  const navigate = useNavigate();
+
+  const handleVolverClick = (event) => {
+    event.preventDefault();
+    navigate(-1);
+  };
 
   const seleccionarIdioma = (lang) => {
     setIdioma(lang);
@@ -15,26 +22,50 @@ const Encabezado = () => {
   return (
     <header className="w-full bg-[#2B5629] py-3 px-3 flex justify-between items-center relative ">
       {/* Izquierda: Volver */}
-      <a
-        href="https://somosawaq.org"
-        className="text-white font-bold text-xs no-underline z-10"
-      >
-        &lt; Volver a la página de AWAQ
-      </a>
-
+      {onHome && (
+        <a
+          href="https://somosawaq.org"
+          className="text-white font-bold text-xs no-underline z-10"
+        >
+          &lt; Volver a la página de AWAQ
+        </a>
+      )}
+      {!onHome && (
+        <button
+          onClick={handleVolverClick}
+          className="bg-[#2B5629] text-white border-2 border-white px-3.5 py-2 text-base rounded-xl hover:font-bold transition flex items-center justify-center w-[80px]"
+          aria-label="Volver a la página de inicio"
+        >
+          Volver
+        </button>
+      )}
       {/* Centro: Logo */}
-      <div className="absolute left-1/2 transform -translate-x-1/2 z-0">
-        <img src={LogoHeader} alt="Logo AWAQ" className="h-12" />
-      </div>
+      {onHome && (
+        <div className="absolute left-1/2 transform -translate-x-1/2 z-0">
+          <img src={LogoHeader} alt="Logo AWAQ" className="h-12" />
+        </div>
+      )}
 
+      {!onHome && (
+        <div
+          className="absolute left-1/2 transform -translate-x-1/2 z-0"
+          onClick={(event) => {
+            event.preventDefault();
+            navigate("/"); // Redirige a la página de inicio
+          }}
+        >
+          <img src={LogoHeader} alt="Logo AWAQ" className="h-12" />
+        </div>
+      )}
       {/* Derecha: FAQs e Idioma */}
       <div className="flex items-center gap-4 z-10">
         {/* Botón Cases */}
-        <Link to="/CrearCaso"
-          className="bg-[#2B5629] text-white border-2 border-white px-3.5 py-2 rounded-xl transition flex items-center justify-center hover:font-bold hover:text-white/80 transition"
+        <Link
+          to="/CrearCaso"
+          className="bg-[#2B5629] text-white border-2 border-white px-3.5 py-2 rounded-xl flex items-center justify-center hover:font-bold hover:text-white/80 transition"
           aria-label="Crear caso"
         >
-        <Smile className="w-5 h-5" />
+          <Smile className="w-5 h-5" />
         </Link>
 
         {/* Botón FAQs */}
--- EcoGuardians/src/home.js
+++ EcoGuardians/src/home.js
@@ -2,7 +2,7 @@ import { Inicio, Nosotros } from "./InHome/inicio.js";
 import './index.css';
 import Preloader from "./InHome/preloader.js";
 import Encabezado from "./InHome/encabezado.js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Footer from "./InHome/footer.js";
 import Sponsors from "./InHome/sponsors.js";  
 import { AnimatePresence } from "framer-motion";
@@ -11,8 +11,18 @@ import SponsorInfoSection from "./InHome/seccionColabora.js";
 import Donaciones from "./InHome/Donaciones.js";  
 
 const Home = () => {
-  const [cargando, setCargando] = useState(true); 
+  const [cargando, setCargando] = useState(false); 
   const navigate = useNavigate();
+  
+  useEffect(() => {
+    const yaVisito = sessionStorage.getItem("homeVisitado");
+
+    if (!yaVisito) {
+      setCargando(true);
+      sessionStorage.setItem("homeVisitado", "true");
+  
+    }
+  }, []);
 
   return (
     <div className="w-full min-h-screen overflow-x-hidden">
@@ -21,7 +31,7 @@ const Home = () => {
       ) : (
         <AnimatePresence>
             <>
-              <Encabezado />
+              <Encabezado onHome={true}/>
               <Inicio 
                 onBienvenidaComoInvitado={() => navigate("/bienvenidaInvitado")} 
                 onProspecto={() => navigate("/prospecto")}
